be1e8b24cbba7ebb4b26ccad27b0df663ced4e70
Evidhu <vidhu145cs@gmail.com>
2016-11-07 15:07:28 +0530

    index

---
+++ index.php
@@ -0,0 +1,5 @@
+<h1>My Positive mind</h1>
+
+<h2><font color="red">Sorry!!!!! Site is under construction.</font></h2>
+
+<h2><font color="green"> Please be here....! We will be up soon...</font></h2>
